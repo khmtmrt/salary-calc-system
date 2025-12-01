98104a9846d53436c91b55f401fdd37fcb104f99
Marta Khomytska <martakhomytska@gmail.com>
2025-12-01 17:52:49 +0200

    fix deploy routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='/salary-calc-system'>
       <Routes>
         <Route path="/login" element={<Login/>} />
         {/* <Route path="/counter" element={<Counter />} /> */}
